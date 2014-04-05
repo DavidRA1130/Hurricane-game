db99a602413dda24a385c9ff11dbc150ec2a4867
David <DavidRA1130@users.noreply.github.com>
2014-04-05 17:56:40 -0400

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hurricanegame;
+
+/**
+ *
+ * @author David & Alexis
+ */
+public class Value {
+    public static int  groundGrass = 0;
+    public static int groundRock = 1; //enemies will travel on this
+    public static int airAir = -1;
+    public static int airHouse = 0;
+    public static int airTrashCan = 1;
+    public static int airFence = 2;
+    public static int enemyAir = -1;
+    public static int enemyWater = 0;
+    public static int[] deathReward = {5};
+}
